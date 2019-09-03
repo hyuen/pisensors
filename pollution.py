210602d53c66dc7f5b55539bb9e01ee3e179ee98
Hector Yuen <hector.yuen@gmail.com>
2019-09-02 22:32:23 -0700

    add changes to write to rrd

--- pollution.py
+++ pollution.py
@@ -1,8 +1,12 @@
 import asyncio
+#import concurrent.features
 import serial_asyncio
 import serial
 import logging
 import time
+import copy
+import rrdtool
+import os
 
 FORMAT = '%(asctime)-15s {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT)
@@ -13,7 +17,7 @@ sleep_bytes = bytearray([0x42, 0x4d, 0xe4, 0x00, 0x00, 0x01, 0x73])
 wakeup_bytes = bytearray([0x42, 0x4d, 0xe4, 0x00, 0x01, 0x01, 0x74])
 
 FRAME_SZ = 32
-REFRESH_SEC = 60
+REFRESH_SEC = 3 # 60
 
 
 class Measurement:
@@ -37,7 +41,11 @@ class Monitor(asyncio.Protocol):
     def __init__(self):
         asyncio.Protocol.__init__(self)
         self.data_ = bytearray()
-        self.ct_ = 0
+        if not os.path.exists("pm_2_5_ct.rrd"):
+            rrdtool.create("pm_2_5_ct.rrd", "--no-overwrite", "--start", "now", "--step", "5",
+                       "RRA:AVERAGE:0.5:1:600",
+                       "RRA:AVERAGE:0.5:1:600",
+                       "DS:pm_2_5_ct:GAUGE:60:0:65536")
 
     def connection_made(self, transport):
         self.transport = transport
@@ -50,22 +58,22 @@ class Monitor(asyncio.Protocol):
         if data == b'\x42':
             log.debug("frame %d %s", len(self.data_), repr(self.data_))
             if self.data_ and len(self.data_) >= FRAME_SZ:
-                self.process(self.data_)
+                frame_data = copy.copy(self.data_)
+                asyncio.ensure_future(self.process(frame_data))
                 self.data_ = bytearray()
                 self.data_ += data
-                #self.ct_ = self.ct_ + 1
-                #if (self.ct_ >= 1):
-                self.transport.write(sleep_bytes)
-                #log.info("sleeping")
-                time.sleep(REFRESH_SEC)
-                #log.info("Done")
-                self.transport.write(wakeup_bytes)
-                #    self.ct_ = 0
+                asyncio.ensure_future(self.do_sleep())
         else:
             self.data_ += data
 
-    def process(self, data):
-        log.debug("%d %s", len(data), repr(data))
+    async def do_sleep(self):
+        self.transport.write(sleep_bytes)
+        # time.sleep(REFRESH_SEC)
+        await asyncio.sleep(REFRESH_SEC)
+        self.transport.write(wakeup_bytes)
+        
+    async def process(self, data):
+        log.info("%d %s", len(data), repr(data))
         if data[0] != 0x42 or data[1] != 0x4d:
             log.info("skipping %s %s %s", repr(data), data[0], data[1])
             return
@@ -75,9 +83,11 @@ class Monitor(asyncio.Protocol):
         higher = data[::2]
         lower = data[1::2]
         proc_data = [(d1 << 8) + d2 for d1, d2 in zip(higher, lower)]
-
+        if len(proc_data) != 12:
+            log.info("aaa  %s", repr(data))
         log.info("%d %s", len(proc_data), proc_data)
-
+        rrdtool.update("pm_2_5_ct.rrd", "N:%d" % (proc_data[9]))
+        
     def connection_lost(self, exc):
         log.info('port closed')
         asyncio.get_event_loop().stop()
@@ -88,6 +98,8 @@ coro = serial_asyncio.create_serial_connection(loop,
                                                Monitor,
                                                '/dev/serial0',
                                                baudrate=9600)
-loop.run_until_complete(coro)
+transport, protocol = loop.run_until_complete(coro)
 loop.run_forever()
 loop.close()
+
+# asyncio.run(main())
